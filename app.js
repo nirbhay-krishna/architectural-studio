170ea2e199a08313bd98a51d579ce1fa12df9927
nirbhay-krishna <nirbhaybakolia456@gmail.com>
2020-12-11 19:25:11 +0530

    added some files

--- app.js
+++ app.js
@@ -20,17 +20,17 @@ const contact1Schema = new mongoose.Schema({
 const Contact1 = mongoose.model('Contact1', contact1Schema);
 
 
-// app.use(express.static('static', options))
+// app.use(express.static('public', options))
 
 //express specific stuff
-app.use('/static', express.static('static')) //to use static files     
+app.use('/public', express.static('public')) //to use static files     
 app.use(express.urlencoded()) //helps in expressing form data       
 
-// //pug specific stuff
+//pug specific stuff
 app.set('view engine', 'pug') //set the template engine as pug
 app.set('views', path.join(__dirname, 'views')) //set the views directory
 
-// //endpoints
+//endpoints
 app.get('/', (req, res) => {
     const params = {}
     res.status(200).render('index.pug', params)
